961b95dd19cb13f79c229bf70de8c96ec0c2968d
Mljungho <mathias.ljungholm@gmail.com>
2022-02-07 09:56:21 +0100

    Adding recipe cypress fixture. Adding basic test to check for 3 recipe Installing MUI library.

--- cypress/integration/visitorCanVisitMainView.feature.js
+++ cypress/integration/visitorCanVisitMainView.feature.js
@@ -1,10 +1,18 @@
 /* eslint-disable no-undef */
 describe("Visitor can visit the home page.", () => {
   before(() => {
+    cy.intercept("GET", "**/api/recipes", {
+      fixture: "recipeIndexResponse.json",
+    }).as("Recipes.index");
     cy.visit("/");
   });
 
   it('is expected to see "Recipe Hub"', () => {
     cy.get("[data-cy=title]").should("contain.text", "Recipe Hub");
   });
+
+  it("is expected to see a collection of recipes", () => {
+    cy.get("[data-cy=recipes-list]").children().should("have.length", 3);
+  });
+
 });
