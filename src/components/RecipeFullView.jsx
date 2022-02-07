b6de687608a02a72d0927acc78b54b2271b87576
giacoletti <giacoletti92@gmail.com>
2022-02-07 22:36:41 +0100

    Refactoring Ingredients list from Recipe full view in new component

--- src/components/RecipeFullView.jsx
+++ src/components/RecipeFullView.jsx
@@ -1,16 +1,8 @@
-import {
-  styled,
-  Typography,
-  Grid,
-  Paper,
-  TableRow,
-  TableCell,
-  TableBody,
-  Table
-} from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { styled, Typography, Grid, Paper } from "@mui/material";
 import Recipes from "../modules/Recipes";
+import IngredientsList from "./IngredientsList";
 
 const Img = styled("img")({
   margin: "auto",
@@ -27,33 +19,11 @@ const RecipeFullView = () => {
     const data = await Recipes.show(id);
     setRecipe(data.recipe);
   };
+  
   useEffect(() => {
     fetchRecipe();
   }, []);
 
-  const ingredientsList = recipe.ingredients?.map((ingredient, index) => {
-    ingredient.index = index + 1;
-    return (
-      <TableRow
-        key={ingredient.index}
-        sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-      >
-        <TableCell
-          component="th"
-          scope="row"
-          data-cy={`ingredient-name-${ingredient.index}`}
-        >
-          {ingredient.name}
-        </TableCell>
-        <TableCell
-          align="right"
-          data-cy={`ingredient-quantity-${ingredient.index}`}
-        >
-          {`${ingredient.amount} ${ingredient.unit}`}
-        </TableCell>
-      </TableRow>
-    );
-  });
   return (
     <Paper
       sx={{ p: 2, margin: "auto", maxWidth: 500, flexGrow: 1, boxShadow: 3 }}
@@ -71,9 +41,7 @@ const RecipeFullView = () => {
           </Typography>
         </Grid>
         <Grid item xs={12}>
-          <Table sx={{ width: 230 }}>
-            <TableBody data-cy="ingredients-list">{ingredientsList}</TableBody>
-          </Table>
+          <IngredientsList ingredients={recipe.ingredients} />
         </Grid>
         <Grid item>
           <Typography
