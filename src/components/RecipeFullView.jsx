c0a593860c19178b6accb13a88421f77218f56a1
defycab <love.bellinder@gmail.com>
2022-02-13 23:53:14 +0100

    imported condition from PR #20 to only shoe delete button on the current users own recipes. harmonized fullrecipeview to facilitate merge

--- src/components/RecipeFullView.jsx
+++ src/components/RecipeFullView.jsx
@@ -5,7 +5,6 @@ import Recipes from "../modules/Recipes";
 import IngredientsList from "./IngredientsList";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { useSelector } from "react-redux";
-
 import { useNavigate } from "react-router-dom";
 
 const Img = styled("img")({
@@ -55,14 +54,7 @@ const RecipeFullView = () => {
       {showEditDelete && (
         <>
           <Button
-            data-cy="edit-recipe-btn"
-            variant="outlined"
-            color="inherit"
-            onClick={() => navigate(`/recipes/${recipe.id}/edit`)}
-          >
-            Edit
-          </Button>
-          <Button
+            sx={{ margin: 1, ml: 44, flexGrow: 1, boxShadow: 3 }}
             data-cy="delete-btn"
             color="inherit"
             variant="outlined"
@@ -76,7 +68,6 @@ const RecipeFullView = () => {
               {message}
             </Alert>
           )}
-          <ShowFullRecipe />
         </>
       )}
       <Grid container spacing={2}>
@@ -119,59 +110,3 @@ const RecipeFullView = () => {
 };
 
 export default RecipeFullView;
-
-// import React, { useState } from "react";
-// import { useParams } from "react-router-dom";
-// import { Button, Alert } from "@mui/material";
-// import DeleteIcon from "@mui/icons-material/Delete";
-// import { useSelector } from "react-redux";
-// import ShowFullRecipe from "./ShowFullRecipe";
-// import Recipes from "../modules/Recipes";
-// import { useNavigate } from "react-router-dom";
-
-// const RecipeFullView = () => {
-
-// //   const { id } = useParams();
-
-// const deleteRecipe = async () => {
-//   const data = await Recipes.delete(id);
-//   setMessage(data.message);
-//   debugger;
-//   return data;
-// };
-
-// const confirmDelete = (confirm) => {
-//   confirm.stopPropagation();
-//   if (window.confirm("Are you sure you want to delete this recipe?")) {
-//     deleteRecipe();
-//     setTimeout(() => navigate("/my-recipes"), 1500);
-//   }
-// };
-
-//   return (
-//     <>
-//       {currentUser ? (
-//         <>
-{
-  /* <Button
-  onClick={confirmDelete}
-  sx={{ margin: 1, ml: 77, flexGrow: 1, boxShadow: 3 }}
-  color="inherit"
-  data-cy="delete-btn"
-  variant="outlined"
-  startIcon={<DeleteIcon />}
->
-  Delete
-</Button> */
-}
-//           <ShowFullRecipe />
-//           <Alert data-cy="flash-message">{message}</Alert>
-//         </>
-//       ) : (
-//         <ShowFullRecipe />
-//       )}
-//     </>
-//   );
-// };
-
-// export default RecipeFullView;
