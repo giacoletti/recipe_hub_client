56d79720065fd8a6407919b8628d16e500ca42ce
giacoletti <giacoletti92@gmail.com>
2022-02-15 10:19:57 +0100

    Install react-use-action-cable package setup useActionCable and subscribe to NotificationsChannel

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   AppBar,
   Box,
@@ -11,17 +11,36 @@ import {
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import LoginForm from "./LoginForm";
+import { useActionCable, useChannel } from "@aersoftware/react-use-action-cable";
 
 const NavigationBar = () => {
+  const [message, setMessage] = useState();
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { currentUser, errorMessage, showLogin } = useSelector(
     (state) => state
   );
+  const { actionCable } = useActionCable("ws://localhost:3000/cable");
+  const { subscribe } = useChannel(actionCable);
 
   const toggleLogin = () => {
     dispatch({ type: "TOGGLE_LOGIN", payload: !showLogin });
   };
+
+  useEffect(() => {
+    subscribe({
+      channel: "NotificationsChannel"
+    }, {
+      connected() {
+        console.log("Connected to Rails AC");
+      },
+      received(data){
+        // debugger;
+        setMessage(data.message);
+      }
+    });
+  }, []);
+  
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -44,6 +63,7 @@ const NavigationBar = () => {
           >
             Recipe Hub
           </Typography>
+          {message && <h3>{message}</h3>}
           {currentUser ? (
             <>
               <Button
