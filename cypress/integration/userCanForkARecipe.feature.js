d774bc21fe6903833bbcb92e71d15b1f58bc863c
giacoletti <giacoletti92@gmail.com>
2022-02-18 15:17:33 +0100

    Add end2end test to check if user can click on 'Fork' recipe button and fork a recipe to add it to its personal collection of recipes in 'My Recipes' view

---
+++ cypress/integration/userCanForkARecipe.feature.js
@@ -0,0 +1,30 @@
+describe('A user can click on "Fork" button on a Recipe in the main view', () => {
+  before(() => {
+    cy.intercept("GET", "/api/recipes", {
+      fixture: "forkedRecipeMyRecipeResponse"
+    });
+    cy.intercept("GET", "/api/recipes", {
+        fixture: "recipesIndexResponse"
+      }).as("RecipesIndex");
+    cy.intercept("POST", "/api/recipes", {
+        fixture: "forkRecipeResponse"
+      }).as("RecipesFork");
+    cy.visitAndAuthenticate();
+    cy.wait("@RecipesIndex");
+    cy.get("[data-cy=recipe-fork-btn-1]").click();
+  });
+
+  it("is expected to make a POST request", () => {
+    cy.wait("@RecipesFork").its("request.method").should("eq", "POST");
+  });
+
+  it('is expected to redirect the user to "My Recipes" view', () => {
+    cy.url().should("contain", "/my-recipes");
+  });
+
+  it('is expected to display forked recipe in "My Recipes" view', () => {
+    cy.get("[data-cy=recipe-card-1]").within(() => {
+      cy.get("[data-cy=recipe-name-1]").should("contain.text", "Fried rice with kimchi");
+    });
+  });
+});
