99f2244727f87b51c4e408f6fa6f8a1ec8287b23
Elvita Kalvāne <elvita.kalvane@gmail.com>
2022-02-19 11:01:54 +0100

    add second intercept with new comment fields to pass the test

--- cypress/integration/userCanPostAComment.feature.js
+++ cypress/integration/userCanPostAComment.feature.js
@@ -16,12 +16,23 @@ describe("User", () => {
     cy.get("[data-cy=post-comment-btn]").click();
   });
 
-  it("is expected to display posted comment in the comment feed", () => {
-    cy.get("[data-cy=comment-feed]")
-      .children()
-      .first().within(() => {
-        cy.get("[data-cy=comment-user-1]").should("contain", "John Skoglund");
-        cy.get("[data-cy=comment-body-1]").should("contain", "Awesome recipe");
+  describe("posts a comment", () => {
+    before(() => {
+      cy.intercept("GET", "/api/recipes/**", {
+        fixture: "recipesShowWithNewCommentsResponse"
       });
+    });
+    it("is expected to display posted comment in the comment feed", () => {
+      cy.get("[data-cy=comment-feed]")
+        .children()
+        .first()
+        .within(() => {
+          cy.get("[data-cy=comment-user-1]").should("contain", "John Skoglund");
+          cy.get("[data-cy=comment-body-1]").should(
+            "contain",
+            "Awesome recipe"
+          );
+        });
+    });
   });
 });
