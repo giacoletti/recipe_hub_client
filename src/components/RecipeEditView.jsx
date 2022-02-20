2c769131d664c45bea425330235b66868b96b64b
giacoletti <giacoletti92@gmail.com>
2022-02-20 21:30:26 +0100

    Apply styling like Recipe create form to Recipe Edit view

--- src/components/RecipeEditView.jsx
+++ src/components/RecipeEditView.jsx
@@ -1,16 +1,27 @@
 import React, { useState, useEffect } from "react";
-import { TextField, Button, Alert, Typography } from "@mui/material";
+import { useParams } from "react-router-dom";
 import Recipes from "../modules/Recipes";
 import utilities from "../modules/utilities";
-import { styled } from "@mui/material/styles";
 import IngredientsFields from "./IngredientsFields";
-import { useParams } from "react-router-dom";
+import {
+  Container,
+  Box,
+  TextField,
+  Button,
+  Alert,
+  Typography,
+  styled
+} from "@mui/material";
+import {
+  PhotoCamera as PhotoCameraIcon,
+  Save as SaveIcon
+} from "@mui/icons-material";
 
 const Input = styled("input")({
   display: "none"
 });
 
-const RecipeCreateForm = () => {
+const RecipeEditView = () => {
   const { id } = useParams();
   const fields = { ingredient_id: "", unit: "", amount: "" };
   const [recipe, setRecipe] = useState({});
@@ -44,7 +55,9 @@ const RecipeCreateForm = () => {
   const updateRecipe = async () => {
     const data = await Recipes.update(recipe);
     setMessage(data.message);
-    setTimeout(() => { setMessage("") }, 4000);
+    setTimeout(() => {
+      setMessage("");
+    }, 4000);
   };
 
   useEffect(() => {
@@ -52,75 +65,106 @@ const RecipeCreateForm = () => {
   }, []);
 
   return (
-    <div>
-      <TextField
-        id="outlined-basic"
-        label="Recipe name"
-        variant="outlined"
-        data-cy="recipe-name"
-        name="name"
-        InputLabelProps={{
-          shrink: true
+    <Container style={{ margin: "auto", width: "50%" }}>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 2 },
+          boxShadow: 3,
+          paddingRight: 4,
+          marginTop: 2
         }}
-        value={recipe.name}
-        onChange={handleChange}
-      />
-      <div>
+        noValidate
+        autoComplete="off"
+      >
         <TextField
-          id="outlined-basic"
-          label="Instructions"
+          data-cy="recipe-name"
+          name="name"
+          value={recipe.name}
+          onChange={handleChange}
+          label="Recipe name"
+          fullWidth
+          size="small"
+          variant="outlined"
+          InputLabelProps={{
+            shrink: true
+          }}
+        />
+        <TextField
+          data-cy="instructions"
+          name="instructions"
           value={recipe.instructions}
+          onChange={handleChange}
+          fullWidth
+          size="small"
+          label="Instructions"
           multiline
           rows={4}
           variant="outlined"
           InputLabelProps={{
             shrink: true
           }}
-          data-cy="instructions"
-          name="instructions"
-          onChange={handleChange}
         />
-      </div>
-      <div>
+        <Typography
+          data-cy="ingredients-label"
+          variant="body1"
+          gutterBottom
+          component="div"
+        >
+          Ingredients
+        </Typography>
         <IngredientsFields
           fields={fields}
           inputList={inputList}
           setInputList={setInputList}
         />
-      </div>
-      <Button variant="outlined" data-cy="submit-btn" onClick={updateRecipe}>
-        Save
-      </Button>
-      <div>
-        <label htmlFor="contained-button-file">
-          <Input
-            id="contained-button-file"
-            data-cy="attach-image"
-            accept="image/*"
-            onChange={handleImage}
-            name="image"
-            multiple
-            type="file"
-          />
-          <Button variant="contained" component="span">
-            Image
-          </Button>
-        </label>
-        <Typography
-          variant="caption"
-          gutterBottom
-          style={{ marginLeft: "10px" }}
+        <div>
+          <label htmlFor="contained-button-file">
+            <Input
+              id="contained-button-file"
+              data-cy="attach-image"
+              accept="image/*"
+              onChange={handleImage}
+              name="image"
+              multiple
+              type="file"
+            />
+            <Button
+              startIcon={<PhotoCameraIcon />}
+              variant="contained"
+              component="span"
+              color="success"
+              style={{ margin: 10 }}
+            >
+              Image
+            </Button>
+          </label>
+          <Typography
+            variant="caption"
+            gutterBottom
+            style={{ marginLeft: "10px" }}
+          >
+            {fileName}
+          </Typography>
+        </div>
+        <Button
+          data-cy="submit-btn"
+          onClick={updateRecipe}
+          endIcon={<SaveIcon />}
+          variant="contained"
+          color="secondary"
+          style={{ marginLeft: 26 }}
         >
-          {fileName}
-        </Typography>
-      </div>
-      {message && (
-        <Alert data-cy="flash-message" severity="info">
-          {message}
-        </Alert>
-      )}
-    </div>
+          Save
+        </Button>
+        {message && (
+          <Alert data-cy="flash-message" severity="info">
+            {message}
+          </Alert>
+        )}
+      </Box>
+    </Container>
   );
 };
 
-export default RecipeCreateForm;
+export default RecipeEditView;
